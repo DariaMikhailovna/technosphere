9a9c5338cd02bd78bb7b95b360a7c64afcb94b57
MargoAddams <0610-1994@mail.ru>
2021-04-19 22:25:10 +0300

    удалила отдельное приложение юзер

--- HW_4/messenger/users/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-#
-# class User(AbstractUser):
-#     email = models.EmailField(max_length=60, unique=True)
-#     last_name = models.CharField(max_length=60, default=False)
-#     first_name = models.CharField(max_length=60, default=False)
-#     date_joined = models.DateTimeField(auto_now_add=True)
